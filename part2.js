ea88cefe8a66dd91a59be11311b4200dae0b121d
Wojtek Dudek <wdudek82@gmail.com>
2018-07-10 23:49:42 +0200

    Added additional animation on keypress and node spawning on click

--- part2.js
+++ part2.js
@@ -1,5 +1,25 @@
+const body = document.querySelector('body');
 const box = document.querySelector('.box');
 
-box.addEventListener('click', (e) => {
-  e.target.classList.toggle('is-paused');
+function randomColorGenerator() {
+  const letters = '0123456789abcdef';
+  let color = '#';
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+document.querySelector('html').addEventListener('click', (e) => {
+  const newBox = document.createElement('div');
+  newBox.classList.add('box');
+  newBox.style.background = randomColorGenerator();
+
+  body.appendChild(newBox);
+});
+
+document.querySelector('html').addEventListener('keydown', () => {
+  body.classList.toggle('rotate');
 });
